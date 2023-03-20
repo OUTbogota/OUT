efaba3f1248c75f43477ce61693726948526f7c4
kafkka <jvelasquezch@unal.edu.co>
2023-03-20 16:13:28 -0500

    Controlador de universidades

--- app/Controllers/Http/UniversidadesController.ts
+++ app/Controllers/Http/UniversidadesController.ts
@@ -1,13 +1,41 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Universidad from 'App/Models/Universidad'
 
 export default class UniversidadesController {
-  public async index({}: HttpContextContract) {}
+  public async index({response}: HttpContextContract) {
+    try{
+      const universidad = await Universidad.all()
+      response.ok(universidad)
+    }
+    catch(e){
+      response.badRequest({mensaje:'Ocurrió un error'})
+    }
+  }
 
   public async create({}: HttpContextContract) {}
 
-  public async store({}: HttpContextContract) {}
+  public async store({response,request}: HttpContextContract) {
+    try {
+      const universidad = request.all()
+      await Universidad.create(universidad)
+      response.ok({msg:'El registro se creó correctamente', data: universidad})
+    }
+    catch(e){
+      response.badRequest({ mensaje:'La universidad ya existe en la base de datos'})
+    }
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async show({response,params}: HttpContextContract) {
+    try{
+      const universidad = await Universidad.findOrFail(params.nombre_universidad)
+
+      response.ok({ mensaje: "Se encontró", data: universidad})
+    }
+    catch(e){
+      response.badRequest({ mensaje: "No se encontró universidad con este nombre"})
+    }
+  }
+  
 
   public async edit({}: HttpContextContract) {}
 
