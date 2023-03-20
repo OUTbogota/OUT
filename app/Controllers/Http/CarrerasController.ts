8e1bb2d8897a73340cf82ce41f560c295977e7aa
Laura Valencia <lvalenciau@unal.edu.co>
2023-03-19 20:01:23 -0500

    Controladores carreras

--- app/Controllers/Http/CarrerasController.ts
+++ app/Controllers/Http/CarrerasController.ts
@@ -1,13 +1,35 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Carrera from 'App/Models/Carrera'
+import { request } from 'http'
 
 export default class CarrerasController {
   public async index({}: HttpContextContract) {}
 
   public async create({}: HttpContextContract) {}
 
-  public async store({}: HttpContextContract) {}
+  public async store({response, request}: HttpContextContract) {
 
-  public async show({}: HttpContextContract) {}
+    try {
+      const carrera = request.all()
+      await Carrera.create(carrera)
+      response.ok({msg:'El registro se creó correctamente', data: carrera})
+    }
+    catch(e){
+      response.badRequest('La carrera ya existe en la base de datos')
+    }
+
+  }
+
+  public async show({response, params}: HttpContextContract) {
+
+    try {
+      const carrera = await Carrera.findOrFail(params.nombre_carrera)
+      response.ok({msg:'El registro se halló correctamente', data: carrera})
+    }
+    catch(e){
+      response.badRequest('No se encontró carrera con ese nombre')
+    }
+  }
 
   public async edit({}: HttpContextContract) {}
 
