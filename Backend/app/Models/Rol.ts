22bec1fd9e19d239f0e818aef5662f1eba62cb25
4l3jo <dsepulvedaa@unal.edu.co>
2023-04-09 21:49:32 -0500

    Cambios en modelo rol

--- Backend/app/Models/Rol.ts
+++ Backend/app/Models/Rol.ts
@@ -2,6 +2,7 @@ import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 
 export default class Rol extends BaseModel {
+  static table = 'roles'
   @column({ isPrimary: true })
   public id: number
 
